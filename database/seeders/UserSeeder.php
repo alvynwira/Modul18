2d584147de2d8abf6881cc3d5c4367220b8f7105
alvyn <alvynwira@student.telkomuniversity.ac.id>
2024-12-29 13:58:37 +0700

    userseeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -12,13 +12,5 @@ class UserSeeder extends Seeder
      * Run the database seeds.
      */
     public function run(): void
-    {
-        DB::table('users')->insert([
-            [
-                'name' => 'Administrator',
-                'email' => 'admin@admin',
-                'password' => bcrypt('adminadmin')
-            ]
-        ]);
-    }
+    
 }
